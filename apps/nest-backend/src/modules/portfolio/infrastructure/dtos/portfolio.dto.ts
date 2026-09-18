6598de02a26efabbc2b99d0f016b9c05a0eb516f
Antrax <kazapox@gmail.com>
2026-09-18 20:11:23 -0300

    fix(portfolio): devolver el implements a PortfolioDto

--- apps/nest-backend/src/modules/portfolio/infrastructure/dtos/portfolio.dto.ts
+++ apps/nest-backend/src/modules/portfolio/infrastructure/dtos/portfolio.dto.ts
@@ -1,8 +1,8 @@
 import { ProfileDto } from '../../../profile/infrastructure/dtos/profile.dto';
-import { ProjectDto, ProjectPageDto } from '../../../project/infrastructure/dtos/project.dto';
+import { ProjectPageDto } from '../../../project/infrastructure/dtos/project.dto';
 import { PortfolioProps } from '../../domain/interfaces/portfolio.interface';
 
-export class PortfolioDto {
+export class PortfolioDto implements PortfolioProps {
   username: string;
   profile: ProfileDto;
   projects: ProjectPageDto;
